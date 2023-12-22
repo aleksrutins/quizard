17d3cbbda47441cb5d2cdd95c283fc0c01f256f0
Aleks Rūtiņš <aleks@rutins.com>
2023-12-21 21:23:18 -0500

    Better header styling

--- app/components/page-header.tsx
+++ app/components/page-header.tsx
@@ -6,7 +6,9 @@ export default (({ title, children }) => {
         <h1 className="text-2xl font-display">{title}</h1>
         <div className="flex flex-row items-center gap-2">
             {children}
-            <UserButton/>
+            <div className="overflow-hidden h-8 w-8 rounded-full bg-stone-300">
+                <UserButton/>
+            </div>
         </div>
     </div>
 }) satisfies FC<{title: string, children?: ReactNode}>;
